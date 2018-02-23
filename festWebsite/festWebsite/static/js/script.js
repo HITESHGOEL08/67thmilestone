c4f7101fdf7a75eb49be4856e94ef3dc4a7edd53
HITESHGOEL08 <hiteshgoel426@gmail.com>
2018-02-23 23:49:46 +0530

    final changes in initial website

--- festWebsite/festWebsite/static/js/script.js
+++ festWebsite/festWebsite/static/js/script.js
@@ -1,11 +1,11 @@
 setInterval(function(){
 
     //time to scroll to bottom
-    $("html, body").animate({ scrollTop: $(document).height() }, 30000);
+    $("html, body").animate({ scrollTop: $(document).height() }, 60000);
 
     //scroll to top
     setTimeout(function() {
-       $('html, body').animate({scrollTop:0}, 9000);
+       $('html, body').animate({scrollTop:0}, 11000);
     },2000);//call every 2000 miliseconds
 
-},2000)
+},1000)
