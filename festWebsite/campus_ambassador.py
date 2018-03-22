b581eef3846251991484fa7bc4ea55b5d966400a
HITESHGOEL08 <hiteshgoel426@gmail.com>
2018-03-23 01:08:55 +0530

    final changes

--- festWebsite/campus_ambassador.py
+++ festWebsite/campus_ambassador.py
@@ -27,7 +27,7 @@ def create():
     emailsend = EmailMessage(subject, body, to=['tushar.bhatia.15csc@bml.edu.in', 'sankalp.pasricha.15csc@bml.edu.in',
                                                 'danish.jameel.15csc@bml.edu.in', 'dadu.reddy.15ece@bml.edu.in',
                                                 'manav.gupta.15cse@bml.edu.in', 'astha.sharma.16mec@bml.edu.in',
-                                                'shreya.mathur.15bk@bml.edu.in'])
+                                                'shreya.mathur.15bk@bml.edu.in', 'nishit.garg.15csc@bml.edu.in'])
     path = os.getcwd()
     path += "/Campus_Ambassadors.csv"
     print(path)
